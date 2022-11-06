c066b098aee7fbede2434709be0ab62eef22fa32
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-06 14:35:23 +0300

    feat: add basic page layout, add Material-UI, create type Me

--- src/s2-homeworks/hw01/message-sender/MessageSender.tsx
+++ src/s2-homeworks/hw01/message-sender/MessageSender.tsx
@@ -1,6 +1,32 @@
 import React, {useEffect, useRef, useState} from 'react'
-import { message0 } from '../HW1'
+import {message0} from '../HW1'
 import s from './MessageSender.module.css'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import {makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+
+
+const useStyles = makeStyles({
+    root: {
+        margin: '73px 0 0 0'
+    },
+    bnt: {
+        background: '#0066CC',
+        borderRadius: '20px',
+        width: '120px',
+        height: '40px'
+    },
+    textArea: {
+        width: '80%',
+        height: '40px !important',
+        background: '#F5F7FB',
+        boxShadow: '0px 5px 20px rgba(29, 33, 38, 0.03), 0px 1px 2px rgba(29, 33, 38, 0.1)',
+        borderRadius: '20px',
+        padding: '12px 0 12px 24px'
+    }
+})
 
 // компонента, которая тестирует вашу компоненту (не изменять, any не трогать)
 const MessageSender = (props: any) => {
@@ -38,17 +64,17 @@ const MessageSender = (props: any) => {
     const onKeyDown = (e: any) => {
         e.key === 'Enter' && e.shiftKey && addMessage()
     }
-
+    const classes = useStyles();
     return (
         <>
             {messages.map((m) => (
-                <M key={'message' + m.id} message={m} />
+                <M key={'message' + m.id} message={m}/>
             ))}
 
-            <div id={'hw1-send-message-form'} className={s.sendForm}>
-                <textarea
+            <Grid container id={'hw1-send-message-form'} className={classes.root} justifyContent={'space-around'}>
+                <TextareaAutosize
                     id={'hw1-textarea'}
-                    className={s.textarea}
+                    className={classes.textArea}
                     ref={textareaRef}
 
                     title={'Shift+Enter for send'}
@@ -58,17 +84,17 @@ const MessageSender = (props: any) => {
                     onChange={onChange}
                     onKeyDown={onKeyDown}
                 />
-                <button
+                <Button
                     id={'hw1-button'}
-                    className={s.button}
-
+                    className={classes.bnt}
+                    variant="contained"
                     onClick={addMessage}
                 >
                     {/*текст кнопки могут изменить студенты*/}
                     Send
                     {/**/}
-                </button>
-            </div>
+                </Button>
+            </Grid>
         </>
     )
 }
