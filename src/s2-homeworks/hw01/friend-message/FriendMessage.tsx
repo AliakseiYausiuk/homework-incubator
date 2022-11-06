e1c4fc80f86c3b3c4ac2c39ba2ad38055643935f
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-06 14:36:34 +0300

    feat: add basic page layout, add Material-UI, create type FriendMessage

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,48 +1,74 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
-
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Paper from '@material-ui/core/Paper';
+import {MessageType} from '../HW1';
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+type FriendMessageType = {
+    message: MessageType
+}
+
+const useStyles = makeStyles({
+    avatar: {
+        width: '48px',
+        height: '48px',
+    },
+    userName: {},
+    message: {},
+    time: {
+        textAlign: 'center',
+    },
+    item: {},
+    messageBlock: {
+        margin: '0 0 0 10px',
+        padding: '8px 13px',
+
+    }
+})
+
+const FriendMessage = (props: FriendMessageType) => {
+    const classes = useStyles();
     return (
-        <div
+        <Grid
             id={'hw1-friend-message-' + props.message.id}
             className={s.friendMessage}
         >
-            <div className={s.friendImageAndText}>
-                <img
-                    id={'hw1-friend-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
-                />
-                <div className={s.friendText}>
-                    <div
-                        id={'hw1-friend-name-' + props.message.id}
-                        className={s.friendName}
+            <Grid container>
+                <Grid>
+                    <Avatar
+                        className={classes.avatar}
+                        id={'hw1-friend-avatar-' + props.message.id}
+                        src={props.message.user.avatar}
+                        alt={'Avatar user'}
+                    />
+                    <Typography
+                        id={'hw1-friend-time-' + props.message.id}
+                        className={classes.time}
                     >
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </div>
-                    <pre
-                        id={'hw1-friend-text-' + props.message.id}
-                        className={s.friendMessageText}
-                    >
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </pre>
-                </div>
-            </div>
-            <div
-                id={'hw1-friend-time-' + props.message.id}
-                className={s.friendTime}
-            >
-                {/*создаёт студент*/}
-
-                {/**/}
-            </div>
-        </div>
+                        {props.message.message.time}
+                    </Typography>
+                </Grid>
+                <Grid className={classes.item}>
+                    <Paper elevation={3} className={classes.messageBlock}>
+                        <Typography
+                            id={'hw1-friend-name-' + props.message.id}
+                            className={classes.userName}
+                        >
+                            {props.message.user.name}
+                        </Typography>
+                        <Typography
+                            id={'hw1-friend-text-' + props.message.id}
+                            className={classes.message}
+                        >
+                            {props.message.message.text}
+                        </Typography>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
