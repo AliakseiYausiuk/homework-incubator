22109dacc1f9ea6fb53374e94e238f4be7c984f0
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-12 21:49:29 +0300

    fix: commit

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -75,7 +75,6 @@ const Stand = () => {
             </div>
 
             <div className={s.checkboxes}>
-                {/*чекбокс с текстом:*/}
                 <div>
                     <SuperCheckbox
                         id={'hw4-super-checkbox-with-text'}
