b48d8e2a1fc9912ef47d386c809cccec1de26e73
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-12 21:46:17 +0300

    feat: add Material-UI

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -4,6 +4,7 @@ import Stand from './Stand'
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import {makeStyles} from '@material-ui/core/styles';
 
 /*
 * 1 - понять (и простить) SuperInputText
@@ -11,15 +12,29 @@ import Container from '@material-ui/core/Container';
 * 3 - дописать onChangeCallback в SuperCheckbox.tsx чтоб оба чекбокса работали на стенде
 * 4 - сделать стили в соответствии с дизайном
 * */
+const useStyles = makeStyles({
+    root: {
+        borderTop: '1px solid #D9D9D9',
+        borderBottom: '1px solid #D9D9D9',
+        padding: '24px 0 32px 0'
+    },
+    title: {
+        margin: '39px 0 10px 70px',
+        fontWeight: 600,
+        fontSize: '22px',
+        lineHeight: '27px'
+    },
+
+})
 
 const HW4 = () => {
+    const classes = useStyles();
     return (
         <Grid id={'hw4'}>
-            <Typography className={s2.hwTitle}>Homework #4</Typography>
-            {/*демонстрация возможностей компонент:*/}
-            <Grid className={s2.hw}>
+            <Typography className={classes.title}>Homework #4</Typography>
+            <Grid className={classes.root}>
                 <Container maxWidth={'lg'}>
-                <Stand />
+                    <Stand/>
                 </Container>
             </Grid>
         </Grid>
--- src/s2-homeworks/hw07/HW7.tsx
+++ src/s2-homeworks/hw07/HW7.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import SuperSelect from './common/c5-SuperSelect/SuperSelect'
 import SuperRadio from './common/c6-SuperRadio/SuperRadio'
 import s2 from '../../s1-main/App.module.css'
 import s from './HW7.module.css'
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import {makeStyles} from '@material-ui/core/styles';
 
 /*
 * 1 - в файле SuperSelect.tsx дописать логику функции onChangeCallback
@@ -11,42 +15,54 @@ import s from './HW7.module.css'
 * 4 - сделать стили в соответствии с дизайном
 * */
 
+
+
+
 const arr = [
-    { id: 1, value: 'x' },
-    { id: 2, value: 'y' },
-    { id: 3, value: 'z' },
-] // value может быть изменено
+    {id: 1, value: 'x'},
+    {id: 2, value: 'y'},
+    {id: 3, value: 'z'},
+]
+const useStyles = makeStyles({
+    root: {
+        borderTop: '1px solid #D9D9D9',
+        borderBottom: '1px solid #D9D9D9',
+        padding: '24px 0 32px 0'
+    },
+    title: {
+        margin: '39px 0 10px 70px',
+        fontWeight: 600,
+        fontSize: '22px',
+        lineHeight: '27px'
+    },
+
+})
 
 const HW7 = () => {
     const [value, onChangeOption] = useState(1) // селект и радио должны работать синхронно
-
+const classes = useStyles();
     return (
-        <div id={'hw7'}>
-            <div className={s2.hwTitle}>Homework #7</div>
+        <Grid id={'hw7'}>
+            <Typography className={classes.title}>Homework #7</Typography>
 
-            {/*демонстрация возможностей компонент:*/}
-            <div className={s2.hw}>
-                <div className={s.container}>
-                    <div>
-                        <SuperSelect
-                            id={'hw7-super-select'}
-                            options={arr}
-                            value={value}
-                            onChangeOption={onChangeOption}
-                        />
-                    </div>
-                    <div>
-                        <SuperRadio
-                            id={'hw7-super-radio'}
-                            name={'hw7-radio'}
-                            options={arr}
-                            value={value}
-                            onChangeOption={onChangeOption}
-                        />
-                    </div>
-                </div>
-            </div>
-        </div>
+            <Grid className={classes.root}>
+                <Container maxWidth={'lg'}>
+                    <SuperSelect
+                        id={'hw7-super-select'}
+                        options={arr}
+                        value={value}
+                        onChangeOption={onChangeOption}
+                    />
+                    <SuperRadio
+                        id={'hw7-super-radio'}
+                        name={'hw7-radio'}
+                        options={arr}
+                        value={value}
+                        onChangeOption={onChangeOption}
+                    />
+                </Container>
+            </Grid>
+        </Grid>
     )
 }
 
