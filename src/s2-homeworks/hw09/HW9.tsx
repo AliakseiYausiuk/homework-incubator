802493adf7504e2a6c397f465c231bedaa3d9081
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-13 14:50:07 +0300

    feat: add Material-UI layout

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Clock from './Clock'
 import s2 from '../../s1-main/App.module.css'
+import {makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 /*
 * 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave
@@ -8,16 +12,33 @@ import s2 from '../../s1-main/App.module.css'
 * 3 - в файле Clock.tsx дизэйблить кнопки стоп / старт если таймер не запущен / запущен соответственно
 * 4 - сделать стили в соответствии с дизайном
 * */
+const useStyles = makeStyles({
+    root: {
+        borderTop: '1px solid #D9D9D9',
+        borderBottom: '1px solid #D9D9D9',
+        padding: '24px 0 32px 0'
+    },
+    title: {
+        margin: '39px 0 10px 70px',
+        fontWeight: 600,
+        fontSize: '22px',
+        lineHeight: '27px'
+    },
+
+})
+
 
 const HW9 = () => {
+    const classes = useStyles();
     return (
-        <div id={'hw9'}>
-            <div className={s2.hwTitle}>Homework #9</div>
-
-            <div className={s2.hw}>
-                <Clock />
-            </div>
-        </div>
+        <Grid id={'hw9'}>
+            <Typography className={classes.title}>Homework #9</Typography>
+            <Grid className={classes.root}>
+                <Container maxWidth={'lg'}>
+                    <Clock/>
+                </Container>
+            </Grid>
+        </Grid>
     )
 }
 
