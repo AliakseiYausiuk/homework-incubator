75e18aa467ff4ece0fe29c94aedeb8db015e49c9
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-15 19:39:37 +0300

    feat: add loader

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,14 @@
-import s from './Loader.module.css'
+// import s from './Loader.module.css'
+//
+// export const Loader = () => <div className={s.loader}/>
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+    return (
+        <Box sx={{display: 'flex'}}>
+            <CircularProgress/>
+        </Box>
+    );
+}
