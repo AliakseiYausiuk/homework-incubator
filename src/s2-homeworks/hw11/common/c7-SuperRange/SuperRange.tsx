71aafd810e7b92a5ed45ce08f86905bce3176510
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-15 19:40:56 +0300

    feat: add attribute

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
+    console.log(props.value);
+
+    // const handleChange = (event: any, newValue: number | number[]) => {
+    //     setValue(newValue as number[]);
+    // };
+    //
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
+            valueLabelDisplay="auto"
+            aria-labelledby="range-slider"
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
