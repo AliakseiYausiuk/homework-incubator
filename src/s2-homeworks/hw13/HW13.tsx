8323f7dc21da9e1292af25fb485b58f8eff15c86
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-18 18:54:56 +0300

    feat: add new logic and Material-UI

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -7,18 +7,38 @@ import success200 from './images/200.svg'
 import error400 from './images/400.svg'
 import error500 from './images/500.svg'
 import errorUnknown from './images/error.svg'
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import {makeStyles} from '@material-ui/core/styles';
 
 /*
 * 1 - дописать функцию send
 * 2 - дизэйблить кнопки пока идёт запрос
 * 3 - сделать стили в соответствии с дизайном
 * */
+const useStyles = makeStyles({
+    root: {
+        borderTop: '1px solid #D9D9D9',
+        borderBottom: '1px solid #D9D9D9',
+        padding: '24px 0 32px 0'
+    },
+    title: {
+        margin: '39px 0 10px 70px',
+        fontWeight: 600,
+        fontSize: '22px',
+        lineHeight: '27px'
+    },
+
+})
 
 const HW13 = () => {
+    const classes = useStyles();
     const [code, setCode] = useState('')
     const [text, setText] = useState('')
     const [info, setInfo] = useState('')
     const [image, setImage] = useState('')
+    const [disabled, setDisabled] = useState(false);
 
     const send = (x?: boolean | null) => () => {
         const url =
@@ -30,33 +50,33 @@ const HW13 = () => {
         setImage('')
         setText('')
         setInfo('...loading')
+        setDisabled(true)
 
         axios
             .post(url, {success: x})
             .then((res) => {
-                setCode('Код 200!')
+                setCode(res.status.toString())
                 setImage(success200)
-                // дописать
-
+                setText(res.data.errorText)
+                setDisabled(false)
             })
             .catch((e) => {
-                // дописать
-
+                setCode(e.code)
+                setText(e.message)
+                setDisabled(false);
             })
     }
 
     return (
-        <div id={'hw13'}>
-            <div className={s2.hwTitle}>Homework #13</div>
-
-            <div className={s2.hw}>
-                <div className={s.buttonsContainer}>
+        <Grid id={'hw13'}>
+            <Typography className={classes.title}>Homework №13</Typography>
+            <Grid className={classes.root}>
+                <Container maxWidth={'lg'}  className={s.buttonsContainer}>
                     <SuperButton
                         id={'hw13-send-true'}
                         onClick={send(true)}
                         xType={'secondary'}
-                        // дописать
-
+                        disabled={disabled}
                     >
                         Send true
                     </SuperButton>
@@ -64,8 +84,7 @@ const HW13 = () => {
                         id={'hw13-send-false'}
                         onClick={send(false)}
                         xType={'secondary'}
-                        // дописать
-
+                        disabled={disabled}
                     >
                         Send false
                     </SuperButton>
@@ -73,8 +92,7 @@ const HW13 = () => {
                         id={'hw13-send-undefined'}
                         onClick={send(undefined)}
                         xType={'secondary'}
-                        // дописать
-
+                        disabled={disabled}
                     >
                         Send undefined
                     </SuperButton>
@@ -82,32 +100,31 @@ const HW13 = () => {
                         id={'hw13-send-null'}
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
-                        // дописать
-
+                        disabled={disabled}
                     >
                         Send null
                     </SuperButton>
-                </div>
+                </Container>
 
-                <div className={s.responseContainer}>
-                    <div className={s.imageContainer}>
+                <Grid className={s.responseContainer}>
+                    <Grid className={s.imageContainer}>
                         {image && <img src={image} className={s.image} alt="status"/>}
-                    </div>
+                    </Grid>
 
-                    <div className={s.textContainer}>
-                        <div id={'hw13-code'} className={s.code}>
+                    <Grid className={s.textContainer}>
+                        <Grid id={'hw13-code'} className={s.code}>
                             {code}
-                        </div>
-                        <div id={'hw13-text'} className={s.text}>
+                        </Grid>
+                        <Grid id={'hw13-text'} className={s.text}>
                             {text}
-                        </div>
-                        <div id={'hw13-info'} className={s.info}>
+                        </Grid>
+                        <Grid id={'hw13-info'} className={s.info}>
                             {info}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
