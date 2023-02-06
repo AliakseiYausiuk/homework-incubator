61b40dcd24765d3c0abed8712285345925a8ba00
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-02-06 13:17:49 +0300

    fix: started value

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -41,9 +41,8 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        console.log(typeof e.currentTarget.value)
         onChange && onChange(e)
-        onChangeOption && onChangeOption(+e.currentTarget.value);
+        onChangeOption && onChangeOption(e.currentTarget.value);
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
