6947fa77c98f9a84d75534d9d329b67464713c29
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-02-06 11:38:37 +0300

    fix: styles open menu

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -11,12 +11,14 @@ type PropsType = {
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar + (open ? ' ' + s.open : '')
+    let finalClass = ({isActive}: { isActive: boolean }) => isActive ? s.active : ''
+
     return (
         <>
             {/*затемнение справа от открытого меню*/}
             {open && <div className={s.background} onClick={handleClose}/>}
 
-            <aside className={open ? sidebarClass : sidebarClass + s.active}>
+            <aside className={sidebarClass}>
                 <button className={s.close} onClick={handleClose}>
                     <img
                         src={closeIcon}
@@ -30,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={sidebarClass} // делает студент
+                        className={finalClass}// делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -38,7 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={sidebarClass} // делает студент
+                        className={finalClass} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -46,7 +48,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={sidebarClass} // делает студент
+                        className={finalClass} // делает студент
                     >
                         Junior Plus
                     </NavLink>
